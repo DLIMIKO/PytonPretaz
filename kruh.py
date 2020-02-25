5e45a274620bf4fe262f33ddce0f4648ddbcb439
DLIMIKO <matus.necas@gmail.com>
2020-02-25 14:17:06 +0100

    Pretazovanie a dekoratory

---
+++ kruh.py
@@ -0,0 +1,21 @@
+# Objekty
+
+class Kruh:
+    def __intit__(self, pol):
+        self.polomer = pol
+
+    def __str__(self):
+        return "Kruh ma polomer {}".format(self.polomer)
+
+    """def __mul__ (self, other):
+        return Kruh (self.polomer*other.polomer)"""
+
+    def obsah(self):
+        return self.polomer ** 2 * 3.14
+
+    @property
+    def polomer(self):
+        return self.polomer
+
+Prvy = Kruh(10)
+print(Prvy.obsah())
